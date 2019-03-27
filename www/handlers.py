42422e3fb6ab6e5609029c72962cad699ea91a51
张冬 <dong.zhang@robooster.com.cn>
2019-03-27 17:27:47 +0800

    First try add upload

--- www/handlers.py
+++ www/handlers.py
@@ -9,6 +9,7 @@ import re, time, json, logging, hashlib, base64, asyncio
 
 import markdown2
 
+import os
 from aiohttp import web
 from coroweb import get, post
 from apis import Page, APIValueError, APIResourceNotFoundError
@@ -416,3 +417,71 @@ async def get_twe(*, page='1',request):
         'blogs': blogs,
         '__user__': request.__user__
     }
+
+# upload part
+
+@get('/upload')
+def upload(request):
+    return {
+        '__template__': 'upload.html'
+    }
+
+
+@post('/file/upload')
+def upload_part(request):  # 接收前端上传的一个分片
+    task = request.form.get('task_id')  # 获取文件的唯一标识符
+    chunk = request.form.get('chunk', 0)  # 获取该分片在所有分片中的序号
+    filename = '%s%s' % (task, chunk)  # 构造该分片的唯一标识符
+
+    upload_file = request.files['file']
+    upload_file.save('./upload/%s' % filename)  # 保存分片到本地
+    return {
+        '__template__': 'upload.html'
+    }
+
+
+@get('/file/merge')
+def upload_success(request):  # 按序读出分片内容，并写入新文件
+    target_filename = request.args.get('filename')  # 获取上传文件的文件名
+    task = request.args.get('task_id')  # 获取文件的唯一标识符
+    chunk = 0  # 分片序号
+    with open('./upload/%s' % target_filename, 'wb') as target_file:  # 创建新文件
+        while True:
+            try:
+                filename = './upload/%s%d' % (task, chunk)
+                source_file = open(filename, 'rb')  # 按序打开每个分片
+                target_file.write(source_file.read())  # 读取分片内容写入新文件
+                source_file.close()
+            except IOError:
+                break
+
+            chunk += 1
+            os.remove(filename)  # 删除该分片，节约空间
+
+    return {
+        '__template__': 'upload.html'
+    }
+
+
+@get('/file/list')
+def file_list():
+    files = os.listdir('./upload/')  # 获取文件目录
+    files = map(lambda x: x if isinstance(x, str) else x.decode('utf-8'), files)  # 注意编码
+    return {
+        '__template__': 'list.html',
+        'files': files
+    }
+
+
+get('/file/download/<filename>')
+def file_download(filename):
+    def send_chunk():  # 流式读取
+        store_path = './upload/%s' % filename
+        with open(store_path, 'rb') as target_file:
+            while True:
+                chunk = target_file.read(20 * 1024 * 1024)
+                if not chunk:
+                    break
+                yield chunk
+    r = web.Response()
+    return r(send_chunk(), content_type='application/octet-stream')
